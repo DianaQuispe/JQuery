b72ced9399d016878163036dd85596b1babc1bdf
DianaQuispe <danaalienmas@gmail.com>
2017-08-28 17:08:31 -0500

    avz

---
+++ assets/js/juego.js
@@ -0,0 +1,329 @@
+'use strict';
+
+
+let x;
+let y;
+let yinicial;
+let xinicial;
+let yfinal;
+let xfinal;
+let xtrans=[];
+let ytrans=[];
+
+let teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
+};
+document.addEventListener("keydown", movimiento);
+
+let map1 = [" * * * * * * * * * * * * * * * * * * * * ",
+    " * *           * * *             * *   * ",
+    " * *             W                 *   * ",
+    " *                           *     *   * ",
+    " *         *                       *   * ",
+    " *   * * * *             *         *   * ",
+    " *     *     * *       *   *       *   * ",
+    " *   * I o   * *         *   * * * *   * ",
+    " *     *     * *   * *  **           I * ",
+    " *   *          *        *         * * * ",
+    " *           * ***   *   *     *   * * * ",
+    " *       *      *        * *       * * * ",
+    " *     * *             ***         * * * ",
+    " * *   *           *     *         *   * ",
+    " *     *   *      *      *     *       * ",
+    " * *        *     *      *         *   * ",
+    " * * * * * * * * * * * * * * * * * * * * "];
+
+let  map2=[" * * * * * * * * * * * * * * * * * * * * * * * * * * * * * ",
+    " * *                                 *             * * * * ",
+    " * *   *                             *       *       * * * ",
+    " *   *   *            *              *     * *     *     * ",
+    " *         *           * *           * * *       *   *   * ",
+    " *     * *       *           *       *   *     *     *   * ",
+    " *   *                       *       *   *   *       *   * ",
+    " * *                             *   *   I          W* * * ",
+    " * *              **     * *                             * ",
+    " * *             *     I           *                     * ",
+    " *               *   *         *                     *   * ",
+    " *   *   *     *       *   *                             * ",
+    " *     *                             *                   * ",
+    " *       * *        *    *   *     * * *           *     * ",
+    " *       * *                         *     *       *     * ",
+    " * *             *       *       *                       * ",
+    " *       *     *       *   * *     *     *   * *         * ",
+    " *     *           *  o*   * *   *   *       * * *       * ",
+    " * * *      *          * *         *   *             * * * ",
+    " * * * * * * * * * * * * * * * * * * * * * * * * * * * * * "];
+
+let map3=[" * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * ",
+    " * * * * * * * * * * *                                           ",
+    " *       *  *  *       * *                                W*     ",
+    " *               *     * * *           *                   I     ",
+    " * *       *               * *                                   ",
+    " *    *    * *   I           * *         *   *                   ",
+    " *                   ****      * * * *   *     *         *       ",
+    " *         *                 *           *   *                   ",
+    " *       *                 *   *                                 ",
+    " *         *   *         *       *   *                           ",
+    " *             *         *                                       ",
+    " * *                ** *                                   *     ",
+    " *                **         * *                                 ",
+    " *  **   *   *  **                     * *                 * *   ",
+    " * *   *   *   *       * *                   * *           * *   ",
+    " *                                         *   *                 ",
+    " *               *     *               ***   * *                 ",
+    " *             *               *                                 ",
+    " *                       *         * *                           ",
+    " *o  *       *           *       * *                           * ",
+    " * * *           *                                         *     ",
+    " * * * *                                  *                *     ",
+    " * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * "];
+
+let map4= ["                                                                 ",
+    " *   o       * **  ** *                                  *****    ",
+    "    *                                                        *    ",
+    "    *           **                                           *    ",
+    "   ** **************         **             ****             *    ",
+    "   **          *           * *                              I*  * ",
+    "   **                                        *                    ",
+    "   *****************                         ****                 ",
+    "       *************                                              ",
+    "     ***************   *   W                                      ",
+    "            ** **      **  **                                     ",
+    "                      *    **                                     ",
+    "  *                  *******                                      ",
+    "   *****            **                                      W     ",
+    "   ***        **  **          *                            ***    ",
+    " **************************************************************** ",
+    "   I  **       *           * *                             ***    ",
+    " *            *                    ** * * * *       *      ***    ",
+    "               *        *                           *       *     ",
+    "    *                  ** *                         *    W        ",
+    " *  **                  *           *             *               ",
+    " *  ****                   *                 *    *          ***  ",
+    " *  *******                *                ****  **              ",
+    " *  ****                  **                ***   ****            ",
+    " *  ***       *         W                   *****     *******     ",
+    " **  *        **    *                        ***  ***********     ",
+    " **    **                                    ****************     ",
+    " **     ** *****       *  *****                 **                ",
+    "           *    * ** **    ****                   *               ",
+    " ** W****************    * ****                    *      ******* ",
+    "                                                                  "];
+
+let map5= ["                                                                 ",
+    "                                W                     W          ",
+    "         *                                       *               ",
+    "   *                    *                            *           ",
+    "                                              *                  ",
+    "                                                  *        *     ",
+    "   *                         W            *         *            ",
+    "          *                                    *            *    ",
+    "                                      *                      *   ",
+    "                                           *                     ",
+    "         *                             *                     I W ",
+    "  *            *       *                                         ",
+    "                                                                 ",
+    "                                                   *             ",
+    "    *               **W*************W********             *      ",
+    "          *         *                       *                    ",
+    "                    *   ******W**********   *                  * ",
+    "                    *   *               *   *       *            ",
+    "                    *   *   **********  *   W                    ",
+    "                    *   W   *        *  *   *                    ",
+    "         *          *   *   *        I  *   *                    ",
+    "   *                *   *   ******** *  *   *    W               ",
+    "                    *   *            *  W   *        *           ",
+    "                    *   **************  *   W               ***  ",
+    "          *         *                   *   *               **   ",
+    "     *              *********************   *        *           ",
+    "          *                                 *        *        W  ",
+    "  *                                         *        *        *  ",
+    "      *       ***W*********W****W****W*******        *******  *  ",
+    " o *                                                             ",
+    "                                                                 "] ;
+
+
+let map6=[  "                                                 ",
+    "                                                 ",
+    "                                                 ",
+    "       **o        ***                **W         ",
+    "                  ***                 *          ",
+    "                  *              *               ",
+    "                  *                     *        ",
+    "     *  *                     *   *     *        ",
+    "      * *                    **** *              ",
+    "      *                                 *        ",
+    "                                       **        ",
+    "                         **            **        ",
+    "                          *                      ",
+    "              ** *       ***                     ",
+    "              ***                                ",
+    "              **                  **             ",
+    "       **                         **             ",
+    "       **                         *              ",
+    "         *            **                         ",
+    "                     ***                         ",
+    "                    * **                         ",
+    "                                                 ",
+    "                                                 "];
+
+let mapas = [map1, map2, map3, map4, map5, map6];
+let s=mapas.length-1;
+let mapa = mapas[s];
+let level=1;
+let map = [];
+let isrunning=false;
+
+function iniciar(){
+  map=[];
+  for (let i = 0; i < mapa.length; i++){
+    map[i]=[];
+    for (let j = 0; j < mapa[0].length; j++) {
+      map[i][j]=mapa[i][j];
+      if(map[i][j]=='o'){
+          xinicial=j;
+          yinicial=i;
+      }
+    }
+  }
+}
+
+function generarMapa(map, direccion) {
+  xtrans=[];
+  ytrans=[];
+  yfinal=map.length-1;
+  xfinal=map[0].length-1;
+  jueguito.innerHTML='';
+  let texto=document.createElement('div');
+  texto.innerHTML='Nivel '+level;
+  texto.setAttribute('class','ganador');
+  let tabla = document.createElement('table');
+  tabla.setAttribute('cellspacing','0');
+  for (let i = 0; i < map.length; i++) {
+    let fila = document.createElement('tr');
+    for (let j = 0; j < map[i].length; j++) {
+        let celda = document.createElement('td');
+        if(map[i][j]=='*'){
+          celda.setAttribute('class','pared');
+        } else if(map[i][j]=='o'){
+          x=j;
+          y=i;
+          celda.setAttribute('class','nave');
+          celda.setAttribute('id',direccion);
+        } else if
+         (map[i][j]=='W') {
+          celda.setAttribute('class', 'llegada');
+        } else if (map[i][j]=='I'){
+         celda.setAttribute('class','transporte');
+          xtrans.push(j);
+          ytrans.push(i);
+        }
+        fila.appendChild(celda);
+    }
+    tabla.appendChild(fila);
+  }
+  jueguito.appendChild(texto);
+  jueguito.appendChild(tabla);
+}
+
+function nivel() {
+  isrunning=false;
+  jueguito.className='';
+  if(s<0){
+    ganar();
+    s=mapas.length-1;
+    mapa=mapas[s];
+  } else {
+    level++;
+    mapa=mapas[s];
+    iniciar();
+    generarMapa(map, 'empezar');
+  }
+}
+function ganar() {
+    jueguito.innerHTML='';
+    level=1;
+    let div=document.createElement('div');
+    div.setAttribute('class', 'ganador');
+    let imagen = document.createElement('img');
+    imagen.setAttribute('src','assets/img/nave.png');
+    let p=document.createElement('p');
+    let texto= document.createTextNode('Ganaste!');
+    p.appendChild(texto);
+    div.appendChild(imagen);
+    div.appendChild(p);
+    jueguito.appendChild(div);
+}
+let t;
+let d;
+function move(a, b, direccion)
+{ 
+  isrunning=true;
+  if( map[y+a][x+b]=="*" ){
+      clearTimeout(t);
+      isrunning=false;
+      return;
+  }
+  if( map[y+a][x+b]=="W" ){
+      clearTimeout(t);
+      jueguito.className='rotar';
+      s--;
+      d = setTimeout(nivel, 1500);
+      return;
+  }
+  if( y+a==0 || x+b==0 || y+a==yfinal || x+b==xfinal){
+      reiniciar();
+      clearTimeout(t);
+      return;
+  }
+  if(map[y+a][x+b]==' ' || map[y+a][x+b]=='_'){
+    map[y][x]=' ';
+    map[y+a][x+b]='o';
+  }
+  if(map[y+a][x+b]=='I'){
+    map[y][x]=' ';
+    if(y+a==ytrans[0]){
+      y=ytrans[1];
+      x=xtrans[1];
+    } else {
+      y=ytrans[0];
+      x=xtrans[0];
+    }
+    map[y+a][x+b]='o';
+  }
+  generarMapa(map, direccion);
+
+  t = setTimeout(function(){ move(a, b, direccion) }, 50);
+}
+
+
+function movimiento(evento)
+{
+  if(isrunning==false){
+    switch(evento.keyCode)
+    {
+      case teclas.UP:
+        move(-1, 0, 'up');
+      break;
+      case teclas.DOWN:
+        move(1, 0, 'down');
+      break;
+      case teclas.LEFT:
+        move(0, -1, 'left');
+      break;
+      case teclas.RIGHT:
+        move(0, 1, 'right');
+      break;
+    }
+  }
+}
+
+function reiniciar() {
+  map[y][x]=' ';
+  map[yinicial][xinicial]='o';
+  isrunning=false;
+  generarMapa(map, 'empezar');
+}
---
+++ assets/js/main.js
@@ -0,0 +1,116 @@
+'use strict';
+let principal = $('.principal')[0];
+let juego = $('.juego')[0];
+let h2play = $('.h2-play')[0];
+let jueguito= $('#juego');
+
+
+h2play.onclick = function() {
+    while (juego.firstChild) {
+      juego.removeChild(juego.firstChild);
+    }
+    while (principal.firstChild) {
+      principal.removeChild(principal.firstChild);
+    }
+    principal.className = "nuevito"
+    agregar();
+}
+function agregar() { 
+    principal.innerHTML='';
+    principal.style.height='600px';
+    jueguito.style.display='none';
+    let divGrande = document.createElement("div");
+    divGrande.setAttribute("id","divGrande");
+    let divdeTexto = document.createElement("div");
+    let divdeButtons = document.createElement("div");
+    divGrande.appendChild(divdeTexto);
+    divGrande.appendChild(divdeButtons);
+    divdeButtons.setAttribute("id","divdeButton")
+    let B1 = document.createElement("button");
+    B1.innerHTML = "★ Start Game";
+    B1.setAttribute("onclick", "startGame()");
+    B1.setAttribute("class","button");
+    let B2 = document.createElement("button");
+    B2.innerHTML = "★ Instructions";
+    B2.setAttribute("onclick", "instructions()");
+    B2.setAttribute("class","button");
+    let B3 = document.createElement("button");
+    B3.innerHTML = "★ Credits";
+    B3.setAttribute("onclick", "credits()");
+    B3.setAttribute("class","button");
+    divdeButtons.appendChild(B1);
+    divdeButtons.appendChild(B2);
+    divdeButtons.appendChild(B3);
+    divdeTexto.setAttribute("id","divdeTexto");
+    divdeTexto.innerHTML += "Road Blocks";
+    principal.appendChild(divGrande);
+}
+function startGame() {
+    level=1;
+    s=mapas.length-1;
+    mapa=mapas[s];
+    let B4 = document.createElement("button");
+    B4.setAttribute("id","B4");
+    B4.setAttribute("onclick", "agregar()");
+    B4.innerHTML = "Menu";
+    B4.setAttribute("class","button");
+    jueguito.style.display='block';
+    let nuevito = document.getElementsByClassName("nuevito")[0];
+        while (nuevito.firstChild) {
+      nuevito.removeChild(nuevito.firstChild);
+    }
+    nuevito.appendChild(B4);
+    nuevito.style.height='50px';
+
+    iniciar();
+    generarMapa(map);
+}
+function instructions() {
+    let B4 = document.createElement("button");
+    B4.setAttribute("id","B4");
+    B4.setAttribute("onclick", "agregar()");
+    B4.innerHTML = "Menu";
+    B4.setAttribute("class","button");
+    let divdeParrafo = document.createElement("div");
+    divdeParrafo.style.fontFamily = 'Orbitron';
+    divdeParrafo.setAttribute("class","divdeParrafo");
+    divdeParrafo.innerHTML = "<h1> INSTRUCTIONS<span> &#160; </span> </h1>";
+    divdeParrafo.innerHTML += "<h4> Ayuda a la nave a aterrizar en un planeta, evita chocar y ayudate de los agujeros negros</h4>";
+    divdeParrafo.innerHTML += "<h4> Tienes que usar los botones del teclado para poder jugar. En total hay seis niveles uno mas complicado que el otro... Te deseo mucha suerte! </h4>";
+       let nuevito = document.getElementsByClassName("nuevito")[0];
+     while (nuevito.firstChild) {
+      nuevito.removeChild(nuevito.firstChild);
+    }
+    nuevito.appendChild(B4);
+    nuevito.appendChild(divdeParrafo)
+}
+function credits() {
+    let B4 = document.createElement("button");
+    B4.setAttribute("id","B4");
+    B4.setAttribute("onclick", "agregar()");
+    B4.innerHTML += "Menu";
+    B4.setAttribute("class","button");
+    let divdeCredits = document.createElement("section");
+    divdeCredits.style.fontFamily = 'Orbitron';
+    divdeCredits.setAttribute("class","star-wars");
+    let divdV = document.createElement("div");
+    divdV.setAttribute("class","crawl");
+    let divtitle = document.createElement("div");
+    divtitle.setAttribute("class","title");
+    let h1 = document.createElement("h1");
+    let textoh1 = document.createTextNode("CREDITS");
+    h1.appendChild(textoh1);
+    divtitle.appendChild(h1);
+    let p = document.createElement("p");
+    let textoP = document.createTextNode("Tienes que usar los botones del teclado para poder jugar. En total hay seis niveles uno mas complicado que el otro... Te deseo mucha suerte!");
+    p.appendChild(textoP);
+    divdV.appendChild(divtitle);
+    divdV.appendChild(p);
+    divdeCredits.appendChild(divdV);
+    let nuevito = document.getElementsByClassName("nuevito")[0];
+        while (nuevito.firstChild) {
+      nuevito.removeChild(nuevito.firstChild);
+    }
+         nuevito.appendChild(divdeCredits);
+        nuevito.appendChild(B4);
+}
